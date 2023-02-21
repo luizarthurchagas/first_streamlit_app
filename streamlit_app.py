4cf1136429b8d46a060bfaed5e58a2729efb70d9
luizarthurchagas <125828486+luizarthurchagas@users.noreply.github.com>
2023-02-21 12:10:30 -0300

    STLIT Stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+streamlit.stop()
+
 streamlit.header('Query data from Snowflake')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
