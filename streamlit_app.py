0d6370f395d3be7eedf9b78c1a680787f93d585e
luizarthurchagas <125828486+luizarthurchagas@users.noreply.github.com>
2023-02-21 13:02:22 -0300

    Add function to write a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#create the repetable code block (called function!)
+# Create the repetable code block (called function!)
 def get_fruitvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
-#new Section to display fruitvice api response
+# New Section to display fruitvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -45,13 +45,24 @@ except URLError as e:
     stremlit.error()
 
 streamlit.header('The fruit load list contains:')
-#Snowflake-related functions
+# Snowflake-related functions
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
-# add a nutton to load the fruit
+# Add a nutton to load the fruit
 if streamlit.button('get Fruit load list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
+   
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my.cur:
+      my.cur.execute("insert into fruit_load_list values ('from streamlit')")
+      returns "Thanks for adding" + new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets[snowflake])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
